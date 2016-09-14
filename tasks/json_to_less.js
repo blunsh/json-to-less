ae6249d25a8af117ab56a0a784fb9d879c5eee32
Liubov Bilogurova <lbilogurova@hytrust.com>
2016-09-13 18:13:24 -0700

    Initial commit

---
+++ tasks/json_to_less.js
@@ -0,0 +1,48 @@
+/*
+ * json-to-less
+ * https://github.com/lbilogurova/json-to-less
+ *
+ * Copyright (c) 2016 Liubov Bilogurova
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  
+
+  grunt.registerMultiTask('json_to_less', 'Compile your json files into less file', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.map(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
